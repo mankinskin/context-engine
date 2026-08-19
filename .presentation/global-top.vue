44f109a294bbe6785bb4b35938be7db297be6231
Linus Behrbohm <linusbehrbohm@web.de>
2026-08-20 00:04:00 +0200

    fix(presentation): persistent left-side hierarchical Toc, hide leaking goto dialog

---
+++ .presentation/global-top.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="fixed left-4 top-4 bottom-4 z-40 w-64 overflow-y-auto rounded-lg bg-main/90 backdrop-blur p-3 text-sm shadow-lg print:hidden"
+  >
+    <Toc :columns="1" />
+  </div>
+</template>
